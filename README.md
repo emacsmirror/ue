2d847f6e52fd38b03c8b2ecd953946afc15434e8
Oleksandr Manenko <koornacht@use.startmail.com>
2021-09-07 18:32:45 +0300

    Rename project

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# Projectile Unreal
-
+# ue.el
 ## Synopsis
 
-**Projectile Unreal** is a minor mode for working with [Unreal Engine](https://www.unrealengine.com/) projects in GNU Emacs.
+**ue** is a minor mode for working with [Unreal Engine](https://www.unrealengine.com/) projects in GNU Emacs.
 Internally it is based on [Projectile](https://github.com/bbatsov/projectile).
 
 It means that you can use Projectile's commands for greping (or acking) files, compile projects, etc.
@@ -18,15 +17,7 @@ manually.
 
 #### Manual
 
-Download the [projectile-unreal.el](https://gitlab.com/manenko/projectile-unreal/-/raw/master/projectile-unreal.el) and
-put it to a directory in your `load-path`, e.g. `~/emacs.d`.
-
-To add a directory to the load path, add the following to your initialization file 
-(probably `~/.emacs` or `~/.emacs.d/init.el`, depends on your preferrable setup):
-
-```el
-(add-to-list 'load-path "mypath")
-```
+TBD (In-Progress)
 
 ## Usage
 
@@ -35,16 +26,16 @@ To add a directory to the load path, add the following to your initialization fi
 Use the package as a global mode:
 
 ```el
-(require 'projectile-unreal)
+(require 'ue)
 
-(projectile-unreal-global-mode)
+(ue-global-mode)
 ```
 
-That will turn on the local `projectile-unreal-mode` for the buffers which belong to an Unreal Engine project.
+That will turn on the local `ue-mode` for the buffers which belong to an Unreal Engine project.
 
 Probably you should read Projectile's [README](https://github.com/bbatsov/projectile) on setting up the completion system,
 caching and indexing files.
 
 ## Caveats
 
-I haven't tested the project on GNU/Linux or Windows yet.
+TBD
---
+++ ue.el
@@ -0,0 +1,482 @@
+;;; ue.el --- Minor mode for Unreal Engine projects based on projectile-mode  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021 Oleksandr Manenko
+
+;; Author:    Oleksandr Manenko <seidfzehsd@use.startmail.com>
+;; URL:       https://gitlab.com/unrealemacs/ue.el
+;; Version:   0.0.1
+;; Created:   26 August 2021
+;; Keywords:  unreal engine, projectile
+;; Package-Requires: ((emacs "25.1") (projectile "0.12.0"))
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; To start it for Unreal Engine projects:
+;;    (ue-global-mode)
+;;
+
+;;; Code:
+
+(require 'projectile)
+(require 'json)
+(require 'seq)
+
+;; TODO: *A*ctors
+;;       *I*nterfaces
+;;       *U*Objects
+;;       *F*Classes
+;; TODO: Set current target and save it? Then use that for a compile command.
+;; TODO: Recommend to switch from alien mode. Or set project root to the Source dir?
+;; TODO: Recommend installing ag Emacs package
+;; TODO: Run the editor. We have -game and -server switches
+;; https://docs.unrealengine.com/4.26/en-US/ProductionPipelines/CommandLineArguments/
+;; TODO: Build configuration
+;; https://docs.unrealengine.com/4.26/en-US/ProductionPipelines/DevelopmentSetup/BuildConfigurations/
+;; TODO: Class wizards
+
+
+(defgroup ue nil
+  "A minor mode for Unreal Engine projects based on projectile-mode."
+  :prefix "ue-"
+  :group  'projectile)
+
+(defcustom ue-attributes
+  '("UCLASS"
+    "UDELEGATE"
+    "UENUM"
+    "UFUNCTION"
+    "UINTERFACE"
+    "UMETA"
+    "UPARAM"
+    "UPROPERTY"
+    "USTRUCT")
+  "List of Unreal C++ attributes."
+  :group 'ue
+  :type  '(repeat string))
+
+(defcustom ue-class-specifiers
+  '("Abstract"
+    "AdvancedClassDisplay"
+    "AutoCollapseCategories"
+    "AutoExpandCategories"
+    "Blueprintable"
+    "BlueprintType"
+    "ClassGroup"
+    "CollapseCategories"
+    "Config"
+    "Const"
+    "ConversionRoot"
+    "CustomConstructor"
+    "DefaultToInstanced"
+    "DependsOn"
+    "Deprecated"
+    "DontAutoCollapseCategories"
+    "DontCollapseCategories"
+    "EditInlineNew"
+    "HideCategories"
+    "HideDropdown"
+    "HideFunctions"
+    "Intrinsic"
+    "MinimalAPI"
+    "NoExport"
+    "NonTransient"
+    "NotBlueprintable"
+    "NotPlaceable"
+    "PerObjectConfig"
+    "Placeable"
+    "ShowCategories"
+    "ShowFunctions"
+    "Transient"
+    "Within"
+    "meta")
+  "List of Unreal C++ UCLASS specifiers."
+  :group 'ue
+  :type  '(repeat string))
+
+(defcustom ue-class-metadata-specifiers
+  '("BlueprintSpawnableComponent"
+    "BlueprintThreadSafe"
+    "ChildCannotTick"
+    "ChildCanTick"
+    "DeprecatedNode"
+    "DeprecationMessage"
+    "DisplayName"
+    "DontUseGenericSpawnObject"
+    "ExposedAsyncProxy"
+    "IgnoreCategoryKeywordsInSubclasses"
+    "IsBlueprintBase"
+    "KismetHideOverrides"
+    "ProhibitedInterfaces"
+    "RestrictedToClasses"
+    "ShortToolTip"
+    "ShowWorldContextPin"
+    "UsesHierarchy"
+    "ToolTip")
+  "List of Unreal C++ UCLASS metadata specifiers."
+  :group 'ue
+  :type  '(repeat string))
+
+(defcustom ue-enum-specifiers
+  '("meta")
+  "List of Unreal C++ UENUM specifiers."
+  :group 'ue
+  :type  '(repeat string))
+
+(defcustom ue-enum-metadata-specifiers
+  '("Bitflags"
+    "Experimental"
+    "ScriptName"
+    "ToolTip")
+  "List of Unreal C++ UENUM metadata specifiers."
+  :group 'ue
+  :type  '(repeat string))
+
+(defcustom ue-enum-enumerator-specifiers
+  '("DisplayName"
+    "Hidden"
+    "ToolTip")
+  "List of Unreal C++ UMETA specifiers used for UENUM enumerators."
+  :group 'ue
+  :type  '(repeat string))
+
+(defcustom ue-function-specifiers
+  '("BlueprintAuthorityOnly"
+    "BlueprintCallable"
+    "BlueprintCosmetic"
+    "BlueprintImplementableEvent"
+    "BlueprintNativeEvent"
+    "BlueprintPure"
+    "CallInEditor"
+    "Category"
+    "Client"
+    "CustomThunk"
+    "Exec"
+    "NetMulticast"
+    "Reliable"
+    "SealedEvent"
+    "ServiceRequest"
+    "ServiceResponse"
+    "Server"
+    "Unreliable"
+    "WithValidation"
+    "meta")
+  "List of Unreal C++ UFUNCTION specifiers."
+  :group 'ue
+  :type  '(repeat string))
+
+(defcustom ue-function-metadata-specifiers
+  '("AdvancedDisplay"
+    "ArrayParm"
+    "ArrayTypeDependentParams"
+    "AutoCreateRefTerm"
+    "BlueprintAutocast"
+    "BlueprintInternalUseOnly"
+    "BlueprintProtected"
+    "CallableWithoutWorldContext"
+    "CommutativeAssociativeBinaryOperator"
+    "CompactNodeTitle"
+    "CustomStructureParam"
+    "DefaultToSelf"
+    "DeprecatedFunction"
+    "DeprecationMessage"
+    "DeterminesOutputType"
+    "DevelopmentOnly"
+    "DisplayName"
+    "ExpandEnumAsExecs"
+    "HidePin"
+    "HideSelfPin"
+    "InternalUseParam"
+    "Keywords"
+    "Latent"
+    "LatentInfo"
+    "MaterialParameterCollectionFunction"
+    "NativeBreakFunc"
+    "NotBlueprintThreadSafe"
+    "ShortToolTip"
+    "ToolTip"
+    "UnsafeDuringActorConstruction"
+    "WorldContext")
+  "List of Unreal C++ UFUNCTION metadata specifiers."
+  :group 'ue
+  :type  '(repeat string))
+
+(defcustom ue-delegate-specifiers
+  '("BlueprintAuthorityOnly"
+    "BlueprintCallable"
+    "BlueprintCosmetic"
+    "BlueprintImplementableEvent"
+    "BlueprintNativeEvent"
+    "BlueprintPure"
+    "CallInEditor"
+    "Category"
+    "Client"
+    "CustomThunk"
+    "Exec"
+    "NetMulticast"
+    "Reliable"
+    "SealedEvent"
+    "ServiceRequest"
+    "ServiceResponse"
+    "Server"
+    "Unreliable"
+    "WithValidation"
+    "meta")
+  "List of Unreal C++ UDELEGATE specifiers."
+  :group 'ue
+  :type  '(repeat string))
+
+(defcustom ue-delegate-metadata-specifiers
+  '("AdvancedDisplay"
+    "ArrayParm"
+    "ArrayTypeDependentParams"
+    "AutoCreateRefTerm"
+    "BlueprintAutocast"
+    "BlueprintInternalUseOnly"
+    "BlueprintProtected"
+    "CallableWithoutWorldContext"
+    "CommutativeAssociativeBinaryOperator"
+    "CompactNodeTitle"
+    "CustomStructureParam"
+    "DefaultToSelf"
+    "DeprecatedFunction"
+    "DeprecationMessage"
+    "DeterminesOutputType"
+    "DevelopmentOnly"
+    "DisplayName"
+    "ExpandEnumAsExecs"
+    "HidePin"
+    "HideSelfPin"
+    "InternalUseParam"
+    "Keywords"
+    "Latent"
+    "LatentInfo"
+    "MaterialParameterCollectionFunction"
+    "NativeBreakFunc"
+    "NotBlueprintThreadSafe"
+    "ShortToolTip"
+    "ToolTip"
+    "UnsafeDuringActorConstruction"
+    "WorldContext")
+  "List of Unreal C++ UDELEGATE metadata specifiers."
+  :group 'ue
+  :type  '(repeat string))
+
+(defcustom ue-uparam-specifiers
+  '("DisplayName"
+    "ref")
+  "List of Unreal C++ UPARAM specifiers."
+  :group 'ue
+  :type  '(repeat string))
+
+(defcustom ue-interface-specifiers
+  '("BlueprintType"
+    "DependsOn"
+    "MinimalAPI"
+    "meta")
+  "List of Unreal C++ UINTERFACE specifiers."
+  :group 'ue
+  :type  '(repeat string))
+
+(defcustom ue-interface-metadata-specifiers
+  '("CannotImplementInterfaceInBlueprint")
+  "List of Unreal C++ UINTERFACE metadata specifiers."
+  :group 'ue
+  :type  '(repeat string))
+
+(defcustom ue-property-specifiers
+  '("AdvancedDisplay"
+    "AssetRegistrySearchable"
+    "BlueprintAssignable"
+    "BlueprintAuthorityOnly"
+    "BlueprintCallable"
+    "BlueprintGetter"
+    "BlueprintReadOnly"
+    "BlueprintReadWrite"
+    "BlueprintSetter"
+    "Category"
+    "Config"
+    "DuplicateTransient"
+    "EditAnywhere"
+    "EditDefaultsOnly"
+    "EditFixedSize"
+    "EditInline"
+    "EditInstanceOnly"
+    "Export"
+    "GlobalConfig"
+    "Instanced"
+    "Interp"
+    "Localized"
+    "Native"
+    "NoClear"
+    "NoExport"
+    "NonPIEDuplicateTransient"
+    "NonTransactional"
+    "NotReplicated"
+    "Replicated"
+    "ReplicatedUsing"
+    "RepRetry"
+    "SaveGame"
+    "SerializeText"
+    "SkipSerialization"
+    "SimpleDisplay"
+    "TextExportTransient"
+    "Transient"
+    "VisibleAnywhere"
+    "VisibleDefaultsOnly"
+    "VisibleInstanceOnly"
+    "meta")
+  "List of Unreal C++ UPROPERTY specifiers."
+  :group 'ue
+  :type  '(repeat string))
+
+(defcustom ue-property-metadata-specifiers
+  '("AllowAbstract"
+    "AllowedClasses"
+    "AllowPreserveRatio"
+    "ArrayClamp"
+    "AssetBundles"
+    "BlueprintBaseOnly"
+    "BlueprintCompilerGeneratedDefaults"
+    "ClampMin"
+    "ClampMax"
+    "ConfigHierarchyEditable"
+    "ContentDir"
+    "DisplayAfter"
+    "DisplayName"
+    "DisplayPriority"
+    "DisplayThumbnail"
+    "EditCondition"
+    "EditFixedOrder"
+    "ExactClass"
+    "ExposeFunctionCategories"
+    "ExposeOnSpawn"
+    "FilePathFilter"
+    "GetByRef"
+    "HideAlphaChannel"
+    "HideViewOptions"
+    "InlineEditConditionToggle"
+    "LongPackageName"
+    "MakeEditWidget"
+    "NoGetter")
+  "List of Unreal C++ UPROPERTY metadata specifiers."
+  :group 'ue
+  :type  '(repeat string))
+
+(defcustom ue-struct-specifiers
+  '("Atomic"
+    "BlueprintType"
+    "Immutable"
+    "NoExport"
+    "meta")
+  "List of Unreal C++ USTRUCT specifiers."
+  :group 'ue
+  :type  '(repeat string))
+
+(defcustom ue-struct-metadata-specifiers
+  '("BlueprintSpawnableComponent"
+    "BlueprintThreadSafe"
+    "ChildCannotTick"
+    "ChildCanTick"
+    "DeprecatedNode"
+    "DeprecationMessage"
+    "DisplayName"
+    "DontUseGenericSpawnObject"
+    "ExposedAsyncProxy"
+    "IgnoreCategoryKeywordsInSubclasses"
+    "IsBlueprintBase"
+    "KismetHideOverrides"
+    "ProhibitedInterfaces"
+    "ShortToolTip"
+    "ShowWorldContextPin"
+    "UsesHierarchy"
+    "ToolTip")
+  "List of Unreal C++ USTRUCT metadata specifiers."
+  :group 'ue
+  :type  '(repeat string))
+
+(defcustom ue-generated-body-macro
+  '("GENERATED_BODY"
+    "GENERATED_IINTERFACE_BODY"
+    "GENERATED_UCLASS_BODY"
+    "GENERATED_UINTERFACE_BODY"
+    "GENERATED_USTRUCT_BODY")
+  "List of Unreal C++ GENERATED_*_BODY macros."
+  :group 'ue
+  :type  '(repeat string))
+
+(defvar ue-root-directory ".uemacs"
+  "The directory that is used to indentify Unreal Emacs project root.")
+
+(defvar ue-cache-data (make-hash-table :test 'equal)
+  "A hash table used for caching information about the current project.")
+
+(defun ue-cache-key (key)
+  "Generate a cache key based on the current directory and the given KEY."
+  (format "%s-%s" default-directory key))
+
+(defun ue-project-root ()
+  "Return Unreal Emacs root directory if this file is part
+of the Unreal Emacs project else nil."
+  (let* ((cache-key   (ue-cache-key "root"))
+	 (cache-value (gethash cache-key ue-cache-data)))
+    (or cache-value
+	(ignore-errors
+	  (let ((root (projectile-locate-dominating-file
+		       default-directory
+		       ue-root-directory)))
+	    (puthash cache-key root ue-cache-data)
+	    root)))))
+
+(defun ue-mode-setup ()
+  "Configure 'ue-mode'."
+  (message "(ue-mode-setup)"))
+
+;;;###autoload
+(define-minor-mode ue-mode
+  "Minor mode for Unreal Engine projects based on projectile-mode."
+  :init-value nil
+  :lighter    " ue"
+  (when ue-mode
+    (ue-mode-setup)))
+
+;;;###autoload
+(defun ue-on ()
+  "Enable 'ue-mode' minor mode if this is an Unreal Engine based project."
+  (when (and (projectile-project-p)
+	     (ue-project-root))
+    (ue-mode +1)))
+
+;;;###autoload
+(define-globalized-minor-mode ue-global-mode
+  ue-mode
+  ue-on)
+
+(defun ue-off ()
+  "Disable 'unreal-emacs-mode' minor mode."
+  (ue-mode -1))
+
+(projectile-register-project-type 'ue           (list ue-root-directory)
+				  :project-file ue-root-directory)
+
+(provide 'ue)
+
+;;; ue.el ends here
